89bb96d8643bd222aa0ceb42f09761adaaa6db9b
eltrillos <strillos@miuandes.cl>
2024-06-13 00:06:31 -0400

    added code that manages reseting of all speeds

--- app/socket.py
+++ app/socket.py
@@ -1,5 +1,5 @@
 from . import socketio, mqtt_client
-from .mqtt import send_mqtt_message
+from .mqtt import send_mqtt_message, last_speeds
 
 
 @socketio.on('fanControl')
@@ -18,7 +18,7 @@ def handle_message(message):
         fanId = message_data.get('id')
         fanSpeed = message_data.get('speed')
         fanTopic = 'fanWall/wall/' + fanId
-        send_mqtt_message(fanTopic, fanSpeed, mqtt_client)
+        send_mqtt_message(fanTopic, fanSpeed, mqtt_client,True)
         # Handle command
     elif message_type == 'config_update':
         print('Received config update:', message_data)
@@ -26,8 +26,15 @@ def handle_message(message):
     elif message_type == 'activate':
         if message_data == 'start':
             send_mqtt_message('fanWall/wall/control', 'start', mqtt_client)
+    elif message_type == 'controllerReset':
+        if message_data == 'reset':
+            send_mqtt_message('fanWall/wall/control', 'reset', mqtt_client)
+            for fanId in last_speeds.keys():
+                last_speeds[fanId] = 0
     else:
         print('Unknown message type')
 
 def send_fan_speed(fanId, fanSpeed):
     socketio.emit('fanSpeed', {'id': fanId, 'speed': fanSpeed})
+
+
