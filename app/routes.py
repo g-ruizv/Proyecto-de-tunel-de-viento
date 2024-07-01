1f07c40414ce927d36dd2fdea537d5ccff315824
eltrillos <strillos@miuandes.cl>
2024-07-01 13:38:48 -0400

    added login requirement for index

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,10 @@
-from flask import render_template
-from . import app
+from flask import Blueprint, render_template
+from flask_login import login_required
+from . import app, users
+
+main = Blueprint('main', __name__)
 
 @app.route('/')
+@login_required
 def index():
     return render_template('index.html')
