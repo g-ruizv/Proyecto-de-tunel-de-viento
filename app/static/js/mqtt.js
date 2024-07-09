6186c1b91361111ab821b33d2e3ab11b9bf4bbed
eltrillos <strillos@miuandes.cl>
2024-07-09 16:16:51 -0400

    mqtt fix

--- app/static/js/mqtt.js
+++ app/static/js/mqtt.js
@@ -1,4 +1,4 @@
-var mqttClient = new Paho.MQTT.Client('broker.hivemq.com', 8884, '5a0d6a66b35b476a8653e7edb84528df');
+var mqttClient = new Paho.MQTT.Client('wss//broker.hivemq.com', 8884, '5a0d6a66b35b476a8653e7edb84528df');
 
 mqttClient.onConnectionLost = function (responseObject) {
     if (responseObject.errorCode !== 0) {
