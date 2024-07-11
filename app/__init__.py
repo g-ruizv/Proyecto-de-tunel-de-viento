9f4812a66ab8d9d0381cb56b8e986e0fd718b00c
eltrillos <strillos@miuandes.cl>
2024-07-10 23:55:19 -0400

    postgres migration fix

--- app/__init__.py
+++ app/__init__.py
@@ -1,22 +1,25 @@
+import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 from flask_login import LoginManager
 from flask_bcrypt import Bcrypt
-from sqlalchemy.orm import joinedload
 from flask_socketio import SocketIO
 from flask_cors import CORS
 import paho.mqtt.client as mqttPaho
 import threading
 from dotenv import load_dotenv
-import os
 
-load_dotenv()  # Load environment variables from .env file
+# Load environment variables from .env file
+load_dotenv()
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
 app.config['CORS_HEADERS'] = 'Content-Type'
+
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 login_manager.login_view = 'user.login'
@@ -33,6 +36,18 @@ def load_user(user_id):
     return models.User.query.get(int(user_id))
 
 def create_app():
+    app = Flask(__name__)
+    app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
+    app.config['CORS_HEADERS'] = 'Content-Type'
+    
+    db.init_app(app)
+    migrate.init_app(app, db)
+    bcrypt.init_app(app)
+    login_manager.init_app(app)
+    socketio.init_app(app, cors_allowed_origins="*")
+    cors.init_app(app)
+    
     with app.app_context():
         models.db.create_all()
 
@@ -42,4 +57,5 @@ def create_app():
     app.register_blueprint(main_blueprint)
 
     mqtt.mqtt_start()
+
     return app
