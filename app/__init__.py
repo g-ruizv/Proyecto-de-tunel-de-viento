eec5ac1b59a506beb98856a2ad968ad0ed168c59
eltrillos <strillos@miuandes.cl>
2024-07-11 00:06:18 -0400

    postgres migration fix

--- app/__init__.py
+++ app/__init__.py
@@ -15,7 +15,7 @@ load_dotenv()
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
-app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_POSTGRES_URL')
 app.config['CORS_HEADERS'] = 'Content-Type'
 
 db = SQLAlchemy(app)
