82f3145913daf6fb6780b087c2b84bf76a72fcde
sebastian trillos <strillos@miuandes.cl>
2024-07-15 18:09:02 -0400

    fixed __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -36,17 +36,6 @@ def load_user(user_id):
     return models.User.query.get(int(user_id))
 
 def create_app():
-    app = Flask(__name__)
-    app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
-    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_POSTGRES_URL')
-    app.config['CORS_HEADERS'] = 'Content-Type'
-    
-    db.init_app(app)
-    migrate.init_app(app, db)
-    bcrypt.init_app(app)
-    login_manager.init_app(app)
-    socketio.init_app(app, cors_allowed_origins="*")
-    cors.init_app(app)
     
     with app.app_context():
         models.db.create_all()
